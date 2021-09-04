24db8ee4af262492aed9b21a4670d82f794a3d08
Maneesh Singh <maneesh54321@gmail.com>
2021-09-04 09:13:20 +0530

    Initial commit

---
+++ counselling-data-service/src/main/java/com/vedantu/counselling/data/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.vedantu.counselling.data.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/testMethod")
+    public String testMethod(){
+        return "test String";
+    }
+}
---
+++ counselling-data-service/src/test/java/com/vedantu/counselling/data/CounsellingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedantu.counselling.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounsellingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

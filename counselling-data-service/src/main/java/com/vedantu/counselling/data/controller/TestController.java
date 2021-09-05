3f612a51d082dfee083051c36db3483acd415019
Maneesh Singh <maneesh54321@gmail.com>
2021-09-05 11:12:01 +0530

    Add basic setup for testing with few sample test cases

--- counselling-data-service/src/main/java/com/vedantu/counselling/data/controller/TestController.java
+++ counselling-data-service/src/main/java/com/vedantu/counselling/data/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.vedantu.counselling.data.controller;
 
+import com.vedantu.counselling.data.view.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class TestController {
     @GetMapping("/test/testMethod2")
     public String testMethod2(){
         return "Test String 2";
+    }
+
+    @GetMapping("/test/person")
+    public Person getPerson(){
+        return new Person(1, "Ravi");
     }
 }
---
+++ counselling-data-service/src/main/java/com/vedantu/counselling/data/view/Person.java
@@ -0,0 +1,15 @@
+package com.vedantu.counselling.data.view;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Person {
+    private Integer id;
+    private String name;
+}

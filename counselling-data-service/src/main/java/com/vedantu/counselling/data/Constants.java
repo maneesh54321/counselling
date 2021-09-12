8e03337ccabfccf350a3b85ffe679796e84d659f
Vinay <jainvinayk@gmail.com>
2021-09-12 16:07:32 +0530

    Modified field for rank filters

---
+++ counselling-data-service/src/main/java/com/vedantu/counselling/data/Constants.java
@@ -0,0 +1,7 @@
+package com.vedantu.counselling.data;
+
+public class Constants {
+    public static final int RANK_TYPE_ADVANCE = 1;
+    public static final int RANK_TYPE_MAINS = 2;
+    public static final int RANK_TYPE_BARCH = 3;
+}

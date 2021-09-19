8cb77bf85c30eca3fb9827aec65868730908e9fb
Vinay <jainvinayk@gmail.com>
2021-09-19 14:39:02 +0530

    Added some video links

--- counselling-data-service/src/data/DDL & DMLs/5. inserts - summary.sql
+++ counselling-data-service/src/data/DDL & DMLs/5. inserts - summary.sql
@@ -1,6 +1,6 @@
-INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:01', 'https://www.youtube.com/watch?v=MN8p-Vrn6G0');
-INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:02', 'https://www.youtube.com/watch?v=MN8p-Vrn6G0');
-INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:03', 'https://www.youtube.com/watch?v=MN8p-Vrn6G0');
-INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:04', 'https://www.youtube.com/watch?v=MN8p-Vrn6G0');
+INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:03', 'MN8p-Vrn6G0');
+INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:01', 'uHOnBQsIakw');
+INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:02', 'aiKDTdOvn0E');
+INSERT INTO video VALUES (nextval('video_id_seq'), true, 'video:04', '9svrftrKb2M');
 
 INSERT INTO summary_data VALUES (nextval('summary_id_seq'), 'description', 'disclaimer');
